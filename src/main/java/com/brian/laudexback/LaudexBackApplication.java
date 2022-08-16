c4ee22edefac1a32dc422bde11923689654dc4c2
monofy01 <bri_riva@hotmail.com>
2022-08-16 17:46:47 -0500

    [INIT] init project structure

---
+++ src/main/java/com/brian/laudexback/LaudexBackApplication.java
@@ -0,0 +1,13 @@
+package com.brian.laudexback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaudexBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaudexBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/laudexback/LaudexBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.laudexback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaudexBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
